c858a5bbe1e99e758ef9084c0669d1a5b577a993
rav <rav@d43e5919-7b50-4614-a95a-1017a2390aba>
2011-03-29 22:38:01 +0000

    update to new ubuntu release waits for boot to complete times out wait loops after 100 secs
    git-svn-id: svn+ssh://svn.sw1v.org/svn/sys/trunk/amazon@905 d43e5919-7b50-4614-a95a-1017a2390aba

--- bin/start-instance.sh
+++ bin/start-instance.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 #
-# TODO: touch something when boot has completed and ssh in to check it
 # TODO: make this check ssh host keys? (otherwise we get intermittent
 #       failures)
-# TODO: figure out how to get most recent ubuntu AMI
-# TODO: remove ~/amazon from everywhere
 # TODO: use snapshots rather than an EBS volume, to enable free use of
 #       availability zones (and to enable attaching at image start time)
+# TODO: figure out how to get most recent ubuntu AMI
 
 set -e
 
@@ -22,14 +20,17 @@ trap "cd /; rm -rf -- '$t'" EXIT
 cd "$t"
 
 echo "building user-data..." >&2
-"$toolsdir/write-mime-multipart" --output=userdata.txt "$@"
+"$toolsdir/write-mime-multipart" --output=userdata.txt \
+    "$@" "/etc/amazon/userdata/touch-boot-complete.conf"
 
 #cat userdata.txt >&2
 gzip userdata.txt
 
+# ids come from http://uec-images.ubuntu.com/releases/10.04/release/
+#  ami-f6340182 \
 echo "starting instance..." >&2
 "$EC2_HOME/bin/ec2-run-instances" \
- ami-f6340182 \
+ ami-3d1f2b49 \
  -k awskey \
  --instance-type t1.micro \
  --instance-initiated-shutdown-behavior terminate \
@@ -41,8 +42,14 @@ instance_id=`grep '^INSTANCE' "run-output" | cut -f2`
 echo "instance id: $instance_id" >&2
 
 echo -n "waiting for instance to start" >&2
+a=0
 while state=$(ec2-describe-instances "$instance_id" | grep ^INSTANCE | \
     cut -f6) && [ "$state" = 'pending' ]; do
+    if [ $a -gt 100 ]; then
+	echo -e "\nGave up after 100 secs" >&2
+	exit 1
+    fi
+    let a=a+1
     echo -n "." >&2
     sleep 1
 done
@@ -53,19 +60,33 @@ echo "ip: $ip" >&2
 
 echo -n "attaching volume" >&2
 ec2-attach-volume vol-aca405c5 -i $instance_id -d /dev/sdc >/dev/null
+a=0
 while state=$(ec2-describe-volumes vol-aca405c5 | grep '^ATTACHMENT' | \
     cut -f5) && [ "$state" = 'attaching' ]; do
+    if [ $a -gt 100 ]; then
+	echo -e "\nGave up after 100 secs" >&2
+	exit 1
+    fi
+    let a=a+1
     echo -n "." >&2
     sleep 1
 done
 echo "" >&2
 
-echo -n "waiting for ssh to work" >&2
-while ! ssh -o StrictHostKeyChecking=no -i /etc/amazon/keys/id_rsa -q ubuntu@$ip true; do
-   echo -n . >&2
-   sleep 1
+echo -n "waiting for boot to complete" >&2
+a=0
+while ! ssh -o StrictHostKeyChecking=no -i /etc/amazon/keys/id_rsa -q ubuntu@$ip \
+       test -f /var/run/boot-complete; do
+    if [ $a -gt 100 ]; then
+	echo -e "\nGave up after 100 secs" >&2
+	exit 1
+    fi
+    let a=a+1
+    echo -n . >&2
+    sleep 1
 done
 echo >&2
+
 
 echo "mounting backup drive" >&2
 ssh ubuntu@$ip -i /etc/amazon/keys/id_rsa sudo mount /dev/sdc1 /mnt
@@ -73,5 +94,3 @@ ssh ubuntu@$ip -i /etc/amazon/keys/id_rsa sudo mount /dev/sdc1 /mnt
 # finally, write the results
 echo -e "instance:\t$instance_id"
 echo -e "ip:\t$ip"
-
-
