8badb4d60d959ccebd59513a7c791aa06e7cbb0b
rav <rav@d43e5919-7b50-4614-a95a-1017a2390aba>
2011-04-14 21:23:42 +0000

    script to do backups
    git-svn-id: svn+ssh://svn.sw1v.org/svn/sys/trunk/amazon@909 d43e5919-7b50-4614-a95a-1017a2390aba

---
+++ bin/amazon-backup.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+set -e
+
+. /etc/backup/config
+
+out=`su amazon -c "/usr/local/amazon/start-instance.sh /etc/amazon/userdata/backup-server.yaml /etc/amazon/userdata/init-ssh-key.sh"`
+cd "$out"
+
+instance_id=`cat instance_id`
+ip=`cat ip`
+
+trap 'su amazon -c "/usr/local/amazon/stop-instance.sh '$out'"' EXIT
+
+su amazon -c "/usr/local/amazon/attach-volume.sh ${BACKUP_VOL} $instance_id /dev/sdc"
+
+echo "mounting backup drive" >&2
+ssh -o StrictHostKeyChecking=yes -oUserKnownHostsFile=known_hosts -i/etc/amazon/keys/id_rsa ubuntu@$ip sudo mount /dev/sdc1 /mnt
+
+backup_path="backup@$ip::/mnt"
+echo "running backup to $backup_path"
+
+
+backup()
+{
+    path="$1"; shift
+    conf="$1"; shift
+    args="--terminal-verbosity 5 -v 8 --force \
+          --exclude-globbing-filelist /etc/backup/exclusions.common"
+
+    # the use of process substitution rather than a simple pipe here
+    # is to ensure that we don't get stuck if the ssh fails to start
+    # (hence never returns any output, hence the rdiff client never
+    # sends any data, hence the cstream never notices the ssh has died,
+    # hence the pipe persists).
+    #
+    # rdiff-backup uses subprocess.py, which hardcodes /bin/sh, which 
+    # doesn't support process substitutions. sigh.
+    schema="/bin/bash -c 'ssh -C -oStrictHostKeyChecking=yes -oUserKnownHostsFile=known_hosts \"%s\" rdiff-backup --server < <(cstream -v 1 -t 20000)'"
+
+    if [ -r /etc/backup/exclusions.${conf} ]; then
+        args="$args --exclude-globbing-filelist /etc/backup/exclusions.${conf}"
+    fi
+    
+    dest="${backup_path}/${path}"
+    rdiff-backup $args --remote-schema "$schema" "$@" "$path" "$dest"
+    
+    # remove old backups
+    rdiff-backup --remote-schema "$schema" --force --remove-older-than 1M12h "$dest"
+}
+
+backup /home home --include=/home/mysql/log --exclude=/home/mysql
+backup /etc etc
+backup /usr/local local
+backup /var/lib/cyrus var
+
