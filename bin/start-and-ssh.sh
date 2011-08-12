466155791722e52edcc71d03a8511ac5afd9476e
rav <rav@d43e5919-7b50-4614-a95a-1017a2390aba>
2011-08-12 10:58:02 +0000

    fix /etc/amazon fallback
    git-svn-id: svn+ssh://svn.sw1v.org/svn/sys/trunk/amazon@931 d43e5919-7b50-4614-a95a-1017a2390aba

--- bin/start-and-ssh.sh
+++ bin/start-and-ssh.sh
@@ -3,10 +3,10 @@
 set -e
 
 amazon_dir=$(dirname "$(readlink -f "$0")")
-if [ -d "${amazon_dir}/../etc" ]; then
-    etc_dir="${amazon_dir}/../etc"
-else
+if [ -d "/etc/amazon" ]; then
     etc_dir="/etc/amazon"
+else
+    etc_dir="${amazon_dir}/../etc"
 fi
 
 out=`${amazon_dir}/start-instance.sh ${etc_dir}/userdata/ssh-server.yaml`
