a1a70ac6a24595b1bbbe4117cd178d9a78632f3c
rav <rav@d43e5919-7b50-4614-a95a-1017a2390aba>
2011-08-23 21:21:52 +0000

    add wrap-cronjob
    git-svn-id: svn+ssh://svn.sw1v.org/svn/sys/trunk/amazon@936 d43e5919-7b50-4614-a95a-1017a2390aba

---
+++ bin/wrap-cronjob.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# wrap a command such that output is suppressed unless the command returns an 
+# error
+
+tf=`mktemp`
+
+"$@" >"$tf" 2>&1
+r=$?
+
+if [ "$r" -ne 0 ]; then
+    echo "$1 returned non-zero: $r"
+    cat "$tf"
+fi
+
+exit $r
+
