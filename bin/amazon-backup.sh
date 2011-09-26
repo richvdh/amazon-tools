811cd3b5d82477940796442c25118964c6b2e0f5
rav <rav@d43e5919-7b50-4614-a95a-1017a2390aba>
2011-09-26 12:25:09 +0000

    use and create snapshots
    git-svn-id: svn+ssh://svn.sw1v.org/svn/sys/trunk/amazon@953 d43e5919-7b50-4614-a95a-1017a2390aba

--- bin/amazon-backup.sh
+++ bin/amazon-backup.sh
@@ -8,17 +8,24 @@ amazon_dir=$(dirname "$(readlink -f "$0")")
 
 export AMAZON_ZONE
 
-out=`su amazon -c "${amazon_dir}/start-instance.sh /etc/amazon/userdata/backup-server.yaml /etc/amazon/userdata/backups-ssh-key.sh"`
+snapid_file="~root/backup/snapid"
+snapid=`cat "$snapid_file"`
+
+if [ -z "$snapid" ]; then
+    echo "unable to read snapshot id from $snapid_file" >&2
+    exit 1
+fi
+
+# faith's backup device is a disk; buffy's is a partition...
+BACKUP_DEVICE=${BACKUP_DEVICE:-/dev/sdc}
+
+out=`su amazon -c "${amazon_dir}/start-instance.sh -u /etc/amazon/userdata/backup-server.yaml -u /etc/amazon/userdata/backups-ssh-key.sh -- -b ${BACKUP_DEVICE}=$snapid"`
 cd "$out"
 
 instance_id=`cat instance_id`
 ip=`cat ip`
 
-trap 'su amazon -c "'${amazon_dir}'/stop-instance.sh '$out'"' EXIT
-
-# faith's backup device is a disk; buffy's is a partition...
-BACKUP_DEVICE=${BACKUP_DEVICE:-/dev/sdc}
-su amazon -c "${amazon_dir}/attach-volume.sh ${BACKUP_VOL} $instance_id ${BACKUP_DEVICE}"
+trap 'su amazon -c "'${amazon_dir}'/terminate-instance.sh '$out'"' EXIT
 
 echo "mounting backup drive"
 ssh -o StrictHostKeyChecking=yes -oUserKnownHostsFile=known_hosts -iid_rsa ubuntu@$ip sudo mount /dev/sdc1 /mnt
@@ -56,3 +63,30 @@ backup()
 
 run_backups
 
+# need to shut down the instance before we can take a snapshot
+su amazon -c "'${amazon_dir}'/terminate-instance.sh -s -w '$out'"
+
+# get the volume id
+su amazon -c "'${amazon_dir}'/aws --xml din '$instance_id'" > din.tmp
+vol_id=`perl -ne 'BEGIN {$v=shift}
+   /<blockDeviceMapping>/ and $b=1; next unless $b;
+   /<deviceName>(.*)<\/deviceName>/ and $d=($1 eq $v); next unless $d;
+   if(/<volumeId>(.*)<\/volumeId>/) {print "$1\n"; exit 0}' ${BACKUP_DEVICE} < din.tmp`
+
+echo "creating S3 snapshot of backup volume"
+desc="`hostname -s` backup `date +'%Y%m%d'`"
+su amazon -c "${amazon_dir}/aws --xml csnap '$vol_id' --description '$desc'" > csnap.out
+newsnapid=`cat csnap.out | sed -e '/<snapshotId>/! d' -e 's/.*<snapshotId>//' -e 's/<.*//'`
+
+if [ -z "$newsnapid" ]; then
+    echo "unable to retrieve snapshot id:" >&2
+    cat csnap.out >&2
+    exit 1
+fi
+
+echo "snapshot id: $newsnapid"
+mv "$snapid_file" "${snapid_file}.0"
+echo $newsnapid > "$snapid_file"
+
+echo "deleting old snapshot $snapid"
+su amazon -c "${amazon_dir}/aws" delete-snapshot "$snapid"
