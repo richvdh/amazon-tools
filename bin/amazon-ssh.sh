a01033351851ed5f037549c1dc5ba633b6fd7f54
rav <rav@d43e5919-7b50-4614-a95a-1017a2390aba>
2012-06-09 13:46:14 +0000

    add ability to pass ssh args
    git-svn-id: svn+ssh://svn.sw1v.org/svn/sys/trunk/amazon@1019 d43e5919-7b50-4614-a95a-1017a2390aba

--- bin/amazon-ssh.sh
+++ bin/amazon-ssh.sh
@@ -1,11 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 #
-# amazon-ssh.sh <state-dir>
+# amazon-ssh.sh [-s <ssh opts>] <state-dir> [<command>]
 #
 set -e
 
+temp=`getopt -n "$0" -o "+s:" -- "$@"`
+eval set -- "$temp"
+sshopts=()
+while true; do
+    case "$1" in
+        "-s") sshopts+=("$2"); shift 2 ;;
+        --) shift; break;;
+    esac
+done
+
 if [ -z "$1" -o ! -d "$1" ]; then
-    echo "usage: amazon-ssh.sh <state-dir>" >&2
+    echo "usage: amazon-ssh.sh [-s <ssh opts>] <state-dir> [<command>]" >&2
     exit 1
 fi
 
@@ -15,4 +25,6 @@ shift
 ip=`cat ip`
 exec ssh -oStrictHostKeyChecking=yes \
          -oUserKnownHostsFile=known_hosts \
-         -iid_rsa ubuntu@$ip "$@"
+         -iid_rsa \
+         "${sshopts[@]}" \
+         ubuntu@$ip "$@"
