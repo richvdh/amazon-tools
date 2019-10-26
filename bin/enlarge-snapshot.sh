660edca01f5aea1c02b76fcf9d6581985ad9c257
Richard van der Hoff <richard@matrix.org>
2019-10-26 23:21:32 +0100

    update enlarge-snapshot

--- bin/enlarge-snapshot.sh
+++ bin/enlarge-snapshot.sh
@@ -3,7 +3,7 @@
 # enlarge an amazon ebs snapshot
 #
 # usage:
-#  enlarge-snapshot.sh <snap id> <new size> [<new desc>]
+#  enlarge-snapshot.sh <new size> [<new desc>]
 #
 # new size: in GB
 #
@@ -13,20 +13,22 @@ set -e
 
 # find the tools
 amazon_dir=$(dirname "$(readlink -f "$0")")
+. "${amazon_dir}/functions.sh"
 
-if [ $# -lt 2 ]; then
-    echo "usage: enlarge-snapshot.sh <snap id> <new size> <new desc>" >&2
+if [ $# -lt 1 ]; then
+    echo "usage: enlarge-snapshot.sh <new size in G> [<new desc>]" >&2
     exit 1
 fi
 
-snapid="$1"
-newsize="$2"
-newdesc="$3"
+newsize="$1"
+newdesc="$2"
 
-DEVICE="/dev/xvdc1"
+snapid=`read_snapid`
+
+BACKUP_DEVICE=${BACKUP_DEVICE:-/dev/sdf}
 
 # fire up an ec2 instance which we'll use to run the resize2fs command, with the snapshot attached
-out=`"${amazon_dir}/start-instance.sh" -- -b "$DEVICE=$snapid:$newsize"`
+out=`"${amazon_dir}/start-instance.sh" -- -b "$BACKUP_DEVICE=$snapid:$newsize"`
 
 cd "$out"
 instance_id=`cat instance_id`
@@ -36,10 +38,10 @@ trap 'echo "warning: ec2 instance $instance_id still extant" &>2' EXIT
 
 # resize2fs requires us to run a fsck before it will do its stuff
 echo "running e2fsck..." >&2
-"${amazon_dir}/amazon-ssh.sh" -s "-t" "$out" sudo e2fsck -f -v "$DEVICE"
+"${amazon_dir}/amazon-ssh.sh" -s "-t" "$out" sudo e2fsck -f -v "$BACKUP_DEVICE"
 
 echo "running resize2fs..." >&2
-"${amazon_dir}/amazon-ssh.sh" "$out" sudo resize2fs "$DEVICE"
+"${amazon_dir}/amazon-ssh.sh" "$out" sudo resize2fs "$BACKUP_DEVICE"
 
 # need to shut down the instance before we can take a snapshot.
 # Note that this only stops the instance, rather than terminating it,
@@ -52,7 +54,7 @@ echo "running resize2fs..." >&2
 vol_id=`perl -ne 'BEGIN {$v=shift}
    /<blockDeviceMapping>/ and $b=1; next unless $b;
    /<deviceName>(.*)<\/deviceName>/ and $d=($1 eq $v); next unless $d;
-   if(/<volumeId>(.*)<\/volumeId>/) {print "$1\n"; exit 0}' ${DEVICE} < din.tmp`
+   if(/<volumeId>(.*)<\/volumeId>/) {print "$1\n"; exit 0}' ${BACKUP_DEVICE} < din.tmp`
 rm din.tmp
 
 echo "creating S3 snapshot of resized volume" >&2
@@ -68,9 +70,9 @@ if [ -z "$newsnapid" ]; then
 fi
 rm csnap.out
 
-echo "done." >&2
-
-echo $newsnapid
+echo "snapshot id: $newsnapid"
+mv "$snapid_file" "${snapid_file}.0"
+echo $newsnapid > "$snapid_file"
 
 "${amazon_dir}/terminate-instance.sh" "$out"
 trap - EXIT
