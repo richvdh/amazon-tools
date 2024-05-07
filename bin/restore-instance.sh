a5e8e7a1e80e92a50832ab167b36fa87980534f4
Richard van der Hoff <github@rvanderhoff.org.uk>
2024-05-07 22:59:15 +0100

    Make restore-instance work with modern backups
    Encrypted drives, ssh_master for backup, etc

--- bin/restore-instance.sh
+++ bin/restore-instance.sh
@@ -19,15 +19,31 @@ fi
 
 
 BACKUP_DEVICE=${BACKUP_DEVICE:-/dev/sdf}
-out=`sudo -u amazon "${amazon_dir}/start-instance.sh" -u "${etc_dir}/userdata/ssh-server.yaml" -- -b "${BACKUP_DEVICE}=$snapid"`
+system_backup_device=/dev/xvdf
+
+out=$(sudo -u amazon "${amazon_dir}/start-instance.sh" \
+    -u "${etc_dir}/userdata/backup-server.yaml" \
+    -- --block-device-mappings "DeviceName=${BACKUP_DEVICE},Ebs={SnapshotId=$snapid,VolumeType=gp2}"
+)
 
 cd "$out"
 instance_id=`cat instance_id`
 ip=`cat ip`
 
-echo "mounting backup drive"
-ssh -o StrictHostKeyChecking=yes -oUserKnownHostsFile=known_hosts -iid_rsa admin@$ip sudo mount /dev/xvdf /mnt
+echo "adding ssh key to backup@"
+ssh_key="$(cat id_rsa.pub)"
+echo "$ssh_key" | ssh -S "ssh_control" admin@$ip sudo tee -a "~backup/.ssh/authorized_keys" > /dev/null
 
+if [ -n "$BACKUP_PASSPHRASE" ]; then
+    echo "unlocking encrypted drive"
+    echo -n "$BACKUP_PASSPHRASE" |
+        ssh -S "ssh_control" admin@$ip sudo cryptsetup open "$system_backup_device" crypt_backup
+    system_backup_device=/dev/mapper/crypt_backup
+fi
+
+
+echo "mounting backup drive"
+ssh -S "ssh_control" admin@$ip sudo mount "${system_backup_device}" /mnt
 
 echo "EC2 instance started at $ip; ssh via \"${amazon_dir}/amazon-ssh.sh\" $out."
 echo "shut it down with \"${amazon_dir}/terminate-instance.sh\" $out."
